c6edf8b5af2e9da3b9fed1091f1f07be16111da8
Gy <406631892@qq.com>
2021-07-04 15:26:53 +0800

    实现alert

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,33 +1,65 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
+import ReactDOM from 'react-dom';
 import classnames from 'classnames';
 import './Dialog.scss';
 import Icon from '../Icon/icon';
-import Button from '../Button/Button';
 
 interface Props {
   visible: boolean;
   className?: string;
+  buttons?: ReactElement[];
+  onClose: React.MouseEventHandler;
+  closeOnClickMask?: boolean;
 }
 
 const Dialog: React.FC<Props> = (props) => {
-  const { visible, children, className } = props;
+  const {
+    visible,
+    children,
+    className,
+    buttons,
+    onClose,
+    closeOnClickMask = true,
+  } = props;
   const classes = classnames('g-dialog', className);
-  return visible ? (
+  const onClickClose = (e: React.MouseEvent) => {
+    onClose(e);
+  };
+  const onClickMask = (e: React.MouseEvent) => {
+    closeOnClickMask && onClickClose(e);
+  };
+  const content = visible ? (
     <>
-      <div className="g-dialog-mask" />
+      <div className="g-dialog-mask" onClick={onClickMask} />
       <div className={classes}>
-        <div className="g-dialog-close">
+        <div className="g-dialog-close" onClick={onClickClose}>
           <Icon name="i-close" />
         </div>
         <header className="g-dialog-header">提示</header>
         <main className="g-dialog-content">{children}</main>
-        <footer className="g-dialog-footer">
-          <Button level="main">OK</Button>
-          <Button>Close</Button>
-        </footer>
+        <footer className="g-dialog-footer">{buttons}</footer>
       </div>
     </>
   ) : null;
+  return ReactDOM.createPortal(content, document.body);
 };
 
 export default Dialog;
+
+export const alert = (content: ReactNode) => {
+  const component = (
+    <Dialog
+      visible
+      onClose={() => {
+        ReactDOM.render(React.cloneElement(component, { visible: false }), div);
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+      }}
+    >
+      {content}
+    </Dialog>
+  );
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
--- src/examples/Dialog/DialogPage.tsx
+++ src/examples/Dialog/DialogPage.tsx
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
-import Dialog from '../../components/Dialog/Dialog';
+import Dialog, { alert } from '../../components/Dialog/Dialog';
+import Button from '../../components/Button/Button';
 
 const DialogPage = () => {
   const [x, setX] = useState(false);
+  const buttons = [<Button level="main">OK</Button>, <Button>Close</Button>];
   return (
     <div>
       <button onClick={() => setX(!x)}>button</button>
-      <Dialog visible={x}>Dialog</Dialog>
+      <Dialog
+        visible={x}
+        onClose={() => {
+          setX(false);
+        }}
+        buttons={buttons.map((button, index) => {
+          return React.cloneElement(button, { key: index });
+        })}
+      >
+        Dialog
+      </Dialog>
+      <button onClick={() => alert('我是alert')}>button</button>
     </div>
   );
 };
