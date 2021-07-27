b96588604b58c1520685b8cafb47f0a0096d676b
Gy <406631892@qq.com>
2021-07-27 10:00:38 +0800

    install dumi

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,20 +1,90 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './Toast.scss';
 
-interface Props {}
+interface Props {
+  visible: boolean;
+  autoClose?: boolean;
+  autoCloseDelay?: number;
+  closeButton?: { text: ReactNode; callback?: Function };
+}
 
 const Toast: React.FC<Props> = (props) => {
-  const { children } = props;
-  return <div className="g-toast">{children}</div>;
+  const {
+    children,
+    autoClose = false,
+    autoCloseDelay = 3,
+    closeButton,
+  } = props;
+  const [visible, setVisible] = useState(props.visible);
+  const toast = useRef<HTMLDivElement>(null);
+  const line = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    execAutoClose();
+    updateStyle();
+  });
+  const close = () => {
+    setVisible(false);
+    unmountWrapper();
+    closeButton && closeButton.callback && closeButton.callback();
+  };
+  const execAutoClose = () => {
+    let id: NodeJS.Timer;
+    if (autoClose) {
+      id = setTimeout(() => {
+        close();
+      }, autoCloseDelay * 1000);
+    }
+    return () => {
+      id && clearTimeout(id);
+    };
+  };
+  const updateStyle = () => {
+    if (line.current) {
+      line.current.style.height = `${
+        toast.current?.getBoundingClientRect().height
+      }px`;
+    }
+  };
+  const unmountWrapper = () => {
+    const div = document.getElementById('g-toast-wrapper');
+    div && ReactDOM.unmountComponentAtNode(div);
+    div && div.remove();
+  };
+  const onClickCloseButton = () => {
+    close();
+    closeButton && closeButton.callback && closeButton.callback();
+  };
+  const content = visible ? (
+    <div className="g-toast" ref={toast}>
+      {children}
+      {closeButton && (
+        <>
+          <div className="line" ref={line} />
+          <div onClick={() => onClickCloseButton()} className="g-toast-close">
+            {closeButton.text}
+          </div>
+        </>
+      )}
+    </div>
+  ) : null;
+  return content;
 };
 
 export default Toast;
 
-export const showToast = (message: ReactNode) => {
-  const component = <Toast>{message}</Toast>;
+export const showToast = (
+  message: ReactNode,
+  object?: Omit<Props, 'visible'>
+) => {
+  const component = (
+    <Toast visible {...object}>
+      {message}
+    </Toast>
+  );
   console.log(component);
   const div = document.createElement('div');
+  div.id = 'g-toast-wrapper';
   document.body.appendChild(div);
   ReactDOM.render(component, div);
 };
--- src/examples/Toast/ToastPage.tsx
+++ src/examples/Toast/ToastPage.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toast, { showToast } from '../../components/Toast/Toast';
 
 const ToastPage = () => {
   return (
     <div>
-      <button onClick={() => showToast('我是 message')}>button</button>
+      <button
+        onClick={() =>
+          showToast(
+            '我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message我是 message 完',
+            {
+              closeButton: { text: '知道了' },
+            }
+          )
+        }
+      >
+        button
+      </button>
     </div>
   );
 };
